2f90cfe08c95cf36189182377fe60b56642f2ddb
Shreya-Pathak <shreyapathak9515@instance-2.us-west1-b.c.controllable-codeswitching.internal>
2022-03-26 08:57:24 +0000

    offline eval; finetuning and pretraining scripts

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 python main.py \
---do_train --do_eval --do_predict\
+--do_train --do_eval \
 --source_lang='en' --target_lang='cm' \
---output_dir='models/finetune_hd' \
+--output_dir='models/pretrain_hd' \
 --per_device_train_batch_size=8 \
 --per_device_eval_batch_size=8 \
 --gradient_accumulation_steps=2 \
 --overwrite_output_dir=True \
 --predict_with_generate \
---model_name_or_path='models/pretrain_hd/checkpoint-7200' \
---train_file='/home/shreyapathak9515/controllable_codemixing/cm_data/finetune_hd_train_hi_cm_format.tsv' \
---validation_file='/home/shreyapathak9515/controllable_codemixing/cm_data/finetune_hd_dev_hi_cm_format.tsv' \
---test_file='/home/shreyapathak9515/controllable-codemixing/cm_data/finetune_test_hi_cm_format.tsv' \
+--train_file='/home/shreyapathak9515/controllable_codemixing/cm_data/pretrain_hd_train_hi_cm_format.tsv' \
+--validation_file='/home/shreyapathak9515/controllable_codemixing/cm_data/pretrain_hd_dev_hi_cm_format.tsv' \
 --load_best_model_at_end \
 --metric_for_best_model='loss' \
 --num_train_epochs=10.0 \
