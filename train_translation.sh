e85dec7d26179df0521dc74ee792c3cedb22fe73
Sneha Mondal <snehamondal@google.com>
2022-06-03 05:03:47 +0000

    change epochs and learning rate

--- train_translation.sh
+++ train_translation.sh
@@ -3,19 +3,20 @@
 python train_translation.py \
 --do_train --do_eval --do_predict \
 --source_lang='en' --target_lang='hi' \
+--model_name_or_path='models/translate_pretrained/checkpoint-92000' \
 --output_dir='models/translate_pretrained' \
 --per_device_train_batch_size=8 \
 --per_device_eval_batch_size=8 \
 --gradient_accumulation_steps=2 \
 --overwrite_output_dir=False \
 --predict_with_generate \
---train_file='data/parallel-n/shuffled_translate.parallel' \
---validation_file='data/dev_test/dev.parallel' \
---test_file='data/dev_test/test.parallel' \
+--train_file='data/translation_data/parallel-n/shuffled_translate_all.parallel' \
+--validation_file='data/translation_data/dev_test/dev.parallel' \
+--test_file='data/translation_data/dev_test/test.parallel' \
 --load_best_model_at_end \
 --metric_for_best_model='bleu' \
 --num_train_epochs=5.0 \
---learning_rate=5e-5 \
+--learning_rate=5e-4 \
 --eval_steps=1000 \
 --save_steps=1000 \
 --evaluation_strategy='steps' \
