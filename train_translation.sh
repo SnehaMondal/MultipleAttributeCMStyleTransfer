7db2fd9a5ecb76c6c2b8866f3d541d194815b4d5
Sneha Mondal <snehamondal@google.com>
2022-06-06 17:19:15 +0000

    update data files

--- train_translation.sh
+++ train_translation.sh
@@ -3,16 +3,16 @@
 python train_translation.py \
 --do_predict \
 --source_lang='en' --target_lang='hi' \
---model_name_or_path='models/translate_pretrained/checkpoint-193000' \
---output_dir='models/translate_pretrained' \
+--model_name_or_path='google/mt5-small' \
+--output_dir='models/mt5_trans_cmgen_multitask' \
 --per_device_train_batch_size=8 \
 --per_device_eval_batch_size=8 \
 --gradient_accumulation_steps=2 \
 --overwrite_output_dir=False \
 --predict_with_generate \
---train_file='data/translation_data/parallel-n/shuffled_translate_all.parallel' \
---validation_file='data/translation_data/dev_test/dev.parallel' \
---test_file='data/translation_data/dev_test/test.parallel' \
+--train_file='data/multitask/shuffled_train_annotated.tsv' \
+--validation_file='data/multitask/hi_cm_valid_masked_annotated.tsv' \
+--test_file='data/multitask/hi_cm_test_annotated.tsv' \
 --load_best_model_at_end \
 --metric_for_best_model='bleu' \
 --num_train_epochs=5.0 \
